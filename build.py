1d1f78c6853b3cd55b8fe6794596a6fcf6976da9
Vladimir Looze <woldemar@mimas.ru>
2021-01-27 13:26:18 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 from os import environ, path
+import json
 from conans.client.conan_api import Conan
 from conans.client.command import Command
 from conans.errors import NotFoundException
@@ -26,8 +27,8 @@ def is_package_reference(line):
         return False
     return True
 
-def export_referenced_recipes(conan):
-    have_packages = False
+def packages_from_conanfile_txt():
+    packages = []
     for line in open(environ["CONAN_TXT"], "rb").read().splitlines():
         strline = line.decode("ascii")
         if not is_package_reference(strline):
@@ -37,18 +38,35 @@ def export_referenced_recipes(conan):
             package_ref = strline.split("@")[0]
         else:
             package_ref = strline
+        packages.append(package_ref)
+    return packages
+
+def export_referenced_conanfiles(conan, packages):
+    for package_ref in packages:
         package, version = package_ref.split("/")
 
         conanfile_location = locate_conanfile_for_package(package, version)
         if not conanfile_location:
-            raise RuntimeError("Could not find recipe for package line %s" % strline)
+            raise RuntimeError("Could not find recipe for package line %s" % package_ref)
 
         conan.export([conanfile_location, package + "/" + version + "@_/_"])
-        have_packages = True
-    return have_packages
 
+def list_installed_packages(conan):
+    installed_packages = []
+    conan.search(["--json", "installed.json", "*"])
+    installed = json.load(open("installed.json","r"))
+    for p in installed["results"][0]["items"]:
+        installed_packages.append(p["recipe"]["id"])
+    return installed_packages
 
-if __name__ == "__main__":
+def verify_packages(conan, installed, expected):
+    print(expected)
+    for package in installed:
+        if package not in expected:
+            raise RuntimeError("Package %s is installed but not specified in conanfile.txt" % package)
+        conan.info([package])
+
+def prepare_conan():
     # these interfere with conan commands
     if "CONAN_USERNAME" in environ:
         del environ["CONAN_USERNAME"]
@@ -56,6 +74,7 @@ if __name__ == "__main__":
         del environ["CONAN_CHANNEL"]
 
     conan = Command(Conan())
+
     conan.config(["install", "https://github.com/trassir/conan-config.git"])
 
     # TODO: delete this after https://github.com/trassir/conan-config/pull/11
@@ -64,23 +83,29 @@ if __name__ == "__main__":
     conan.remote(["add", "trassir-staging", "https://api.bintray.com/conan/trassir/conan-staging", "True"])
     conan.remote(["add", "trassir-public", "https://api.bintray.com/conan/trassir/conan-public", "True"])
     conan.remote(["add", "conan-center", "https://conan.bintray.com", "True"])
+    if "CONAN_PASSWORD" in environ:
+        conan.user(["--password", environ["CONAN_PASSWORD"], "--remote", upload_remote, "trassir-ci-bot"])
+    return conan
 
-    if environ.get("GITHUB_HEAD_REF", "master") == "master":
-        upload_remote = "trassir-public"
-    else:
-        upload_remote = "trassir-staging"
 
-    expect_packages = export_referenced_recipes(conan)
+if __name__ == "__main__":
+    conan = prepare_conan()
+
+    expected_packages = packages_from_conanfile_txt()
+    export_referenced_conanfiles(conan, expected_packages)
 
     conan.install([environ["CONAN_TXT"],
                     "-if", "install_dir",
                     "-pr", environ["CONAN_PROFILE"],
                     "-s", "build_type=Release",
                     "--build", "missing"])
-    conan.user(["--password", environ["CONAN_PASSWORD"], "--remote", upload_remote, "trassir-ci-bot"])
-    conan.search(["*"])
-    try:
+
+    installed = list_installed_packages(conan)
+    verify_packages(conan, installed, expected_packages)
+
+    if environ.get("GITHUB_HEAD_REF", "master") == "master":
+        upload_remote = "trassir-public"
+    else:
+        upload_remote = "trassir-staging"
+    if installed:
         conan.upload(["--confirm", "--force", "--all", "-r", upload_remote, "*"])
-    except NotFoundException:
-        if expect_packages:
-            raise
