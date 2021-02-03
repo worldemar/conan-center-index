25b4051dbc249217bf51dd17b274d5f12728cd14
Vladimir Looze <woldemar@mimas.ru>
2021-02-03 12:22:43 +0300

    Update environment.py

--- environment.py
+++ environment.py
@@ -18,8 +18,11 @@ def prepare_environment():
     # TODO: delete this after https://github.com/trassir/conan-config/pull/11
     conan.remote(["remove", "bintray-trassir"])
 
-    conan.remote(["add", "trassir-staging", "https://api.bintray.com/conan/trassir/conan-staging", "True"])
-    conan.remote(["add", "trassir-public", "https://api.bintray.com/conan/trassir/conan-public", "True"])
+    #conan.remote(["add", "trassir-staging", "https://api.bintray.com/conan/trassir/conan-staging", "True"])
+    #conan.remote(["add", "trassir-public", "https://api.bintray.com/conan/trassir/conan-public", "True"])
+    conan.remote(["add", "trassir-staging", "https://api.bintray.com/conan/worldemar/cci-pr", "True"])
+    conan.remote(["add", "trassir-public", "https://api.bintray.com/conan/worldemar/cci-master", "True"])
+
     # conan.remote(["add", "conan-center", "https://conan.bintray.com", "True"])
 
     if environ.get("GITHUB_HEAD_REF", "master") == "master":
