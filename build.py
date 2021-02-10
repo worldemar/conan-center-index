f4a556a663b8909a3746e291d7ab5376050b2467
Vladimir Looze <woldemar@mimas.ru>
2021-02-10 13:43:39 +0300

    Update build.py

--- build.py
+++ build.py
@@ -53,8 +53,8 @@ def detect_updated_packages(master_txt, branch_txt):
 
 if __name__ == "__main__":
     import subprocess
-    subprocess.check_call(['docker', 'run', 'trassiross/conan-gcc8', "pwd"])
-    subprocess.check_call(['docker', 'run', 'trassiross/conan-gcc8', "ls", "-la"])
+    subprocess.check_call(['docker', 'run', 'trassiross/conan-gcc8', '-v', 'sources:sources', "pwd"])
+    subprocess.check_call(['docker', 'run', 'trassiross/conan-gcc8', '-v', 'sources:sources', "ls", "-la"])
     # subprocess.check_call(["git", "--help"])
     sys.exit(1)
 
